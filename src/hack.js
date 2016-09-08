a19fd8d4ea97f08a1ae9da7ff2ae49a678234bcc
teramotodaiki <i101323@gmail.com>
2016-09-08 18:35:14 +0900

    Uncheck parent origin

--- src/hack.js
+++ src/hack.js
@@ -24,6 +24,24 @@ canvas.addEventListener('resize', () => {
 document.body.appendChild(canvas);
 Hack.canvas = canvas; // export as default
 
+// Un-checked parent origin
+const _addEventListener = addEventListener;
+addEventListener = (...args) => {
+  if (args[0] === 'message' && typeof args[1] === 'function') {
+    const _listener = args[1];
+    args[1] = (...eArgs) => {
+      const {data, source} = eArgs[0];
+      if (source === parent) {
+        eArgs[0] = {
+          origin: '*', // Ignore origin check
+          data, source
+        };
+      }
+      return _listener.apply(window, eArgs);
+    };
+  }
+  return _addEventListener.apply(window, args);
+};
 
 // Connect
 const handshake = new Postmate.Model({

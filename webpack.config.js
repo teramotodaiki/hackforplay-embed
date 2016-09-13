baf74a7366eea720774e52a8207a1af735c285d6
teramotodaiki <i101323@gmail.com>
2016-09-13 19:10:21 +0900

    i babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,14 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'public'),
     filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ["babel"],
+        exclude: /node_modules/
+      }
+    ]
   }
 };
